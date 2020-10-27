5bf61c9d86f4f9a1ad41326a9d7db6950f18688d
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-10-27 18:11:15 -0500

    boilerplate. first commit.

---
+++ src/components/GameBoard.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+
+import Data from '../data.json';
+
+const GameBoard = () => {
+
+
+
+
+  return(
+    <div>
+
+    <button>Next Question</button>
+  </div>
+  )
+};
+
+export default GameBoard;
---
+++ src/components/QCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const QCard = () => {
+
+    return
+
+}
+
+export default QCard;
